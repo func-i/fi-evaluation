133b72aebabcfd4b6b95c340ae7dd285ed1d862f
Jonathan Salis <jtsseneca@hotmail.com>
2014-07-03 13:21:29 -0400

    Adding comments and updating README

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 fi-evaluation
 =============
 
-A repo to evaluate juniors
+This app is very simple.
+It is a dice rolling game that uses some cryptographic methods to generate a "Lucky Nunber"
+which will be compared to a random number.  Lucky Number's that are lower are winners, higher, losers!
+
+Your job
+========
+
+* Clean up the app code where you see necessary.
+* Check the game logic for any potential mistakes.
+* There aren't any tests, if you want to write some, feel free.  (Totally optional, only a few tests please)
+* Plenty of functionality is missing, feel free to add a couple of things you feel are missing.
--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,7 +3,9 @@ class GamesController < ApplicationController
   def bet
     @bet = params[:amount].to_i
 
-    if @bet > 0 && @bet > @player.balance
+    # => TODO  There might be a problem with the bet amount
+    # => TODO: Correct any problems with it
+    if (@bet > 0 && @bet > @player.balance)
       @invalid_bet = true
     else
       @secret = SecureRandom.hex(32)
@@ -14,7 +16,6 @@ class GamesController < ApplicationController
       @result_hex = sha256.hexdigest.first(4)
       @result_int = @result_hex.to_i(16)
       @roll = SecureRandom.random_number(65535)
-
 
       if @result_int < @roll
         @player.update(balance: @player.balance + @bet)
